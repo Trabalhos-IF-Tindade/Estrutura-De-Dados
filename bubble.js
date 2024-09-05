2c0ef86e946a45c46fb4a0599ed182061808ff82
Luís Felipe Marques Tomé <113529692+LipeMT@users.noreply.github.com>
2024-09-05 20:01:09 -0300

    Add files via upload

---
+++ bubble.js
@@ -0,0 +1,30 @@
+function bubbleSort(arr) {
+    let trocas = 0;
+    let updates = 0;
+    let comparisons = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < (arr.length - i - 1); j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+                updates++
+                trocas++
+            }
+            comparisons++
+        }
+        if(trocas == 0){
+            break
+        }
+        trocas = 0
+    }
+    console.log(arr);
+    console.log(updates);
+    console.log(comparisons);
+}
+
+// let arr = [234, 43, 55, 63, 5, 6, 235, 547];
+let arr = [1,2,3]
+
+bubbleSort(arr);
