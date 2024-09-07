4d3401720bb4d91acfa8e609ec4812b3ece45aa0
Luis Felipe Tomé <lipetome010@gmail.com>
2024-09-07 11:53:01 -0300

    Convertendo o método bubble.js para um não otimizado

--- Trabalho1/bubble.js
+++ Trabalho1/bubble.js
@@ -3,14 +3,12 @@ export class Bubble {
     constructor() { }
 
     exec(arr) {
-        const inicio =  new Date();
+        console.time('tempoDeExecucao: ')
 
-        let trocas = 0;
         let updates = 0;
         let comparisons = 0;
 
         for (let i = 0; i < arr.length; i++) {
-            //console.log('Rodou');
 
             for (let j = 0; j < (arr.length - i - 1); j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -22,17 +20,11 @@ export class Bubble {
                 }
                 comparisons++
             }
-
-            if (trocas == 0) {
-                break;
-            }
-
-            trocas = 0
         }
 
         console.log('# MÉTODO DA BOLHA #');
         const fim = new Date();
-        console.log('tempoDeExecucao: ', fim - inicio);
+        console.timeEnd('tempoDeExecucao: ');
         console.log(`Trocas: ${updates}`);
         console.log(`Comparações: ${comparisons}`);
     }
